0543502aab97206743362592c71b89324047d19a
yourcoke <deinkoks@gmail.com>
2017-08-09 11:15:23 +0200

    Added buy modal, alerts

--- TanteMateLaden/store/admin.py
+++ TanteMateLaden/store/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Drink
+from .models import Account, Drink
 
+admin.site.register(Account)
 
 @admin.register(Drink)
 class DrinkAdmin(admin.ModelAdmin):
     list_display = ("name", "volume", "alcoholic", "caffeine")
     prepopulated_fields = {"slug": ("name", "volume")}
+
--- TanteMateLaden/store/templatetags/store_widgets.py
+++ TanteMateLaden/store/templatetags/store_widgets.py
@@ -1,5 +1,6 @@
 from django import template
-from store.models import Drink
+from django.contrib.auth.models import AnonymousUser
+from store.models import Drink, Account
 
 register = template.Library()
 
@@ -8,7 +9,15 @@ def simple_drink_list():
 	drinks = Drink.objects.all()
 	return {'drinks': drinks}
 
-@register.inclusion_tag('store/widgets/drink_row.html')
-def drink_row():
+@register.inclusion_tag('store/widgets/drink_row.html', takes_context=True)
+def drink_row(context):
 	drinks = Drink.objects.all()
-	return {'drinks': drinks}
+	return {'drinks': drinks, 'request': context['request']}
+
+@register.inclusion_tag('store/widgets/buy_modal.html', takes_context=True)
+def buy_item_modal(context, item):
+	user = context['request'].user
+	accounts = Account.objects.filter(free_access=True) | Account.objects.exclude(pin=None)
+	if not isinstance(user, AnonymousUser):
+		accounts = accounts.exclude(user = user).order_by('user__username')
+	return {'item': item, 'user': user, 'accounts': accounts}
